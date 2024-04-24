9b20376d63b54c5f0ac906c6a0cd4ae5d3f7b860
yibai <404223879@qq.com>
2024-04-24 21:49:38 +0800

    code63Unique_PathsII

---
+++ dynamic_programming/basic_topic/code63Unique_PathsII.java
@@ -0,0 +1,28 @@
+package algorithmbasic.leetcode.dynamic_programming.basic_topic;
+
+public class code63Unique_PathsII {
+    public static int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        //如果起点或者终点有障碍，直接返回0
+        if (obstacleGrid[0][0] == 1 || obstacleGrid[m - 1][n - 1] == 1) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 1; i < m && obstacleGrid[i][0] == 0; i++) {
+            dp[i][0] = 1;
+        }
+        for (int i = 1; i < n && obstacleGrid[0][i] == 0; i++) {
+            dp[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                if (obstacleGrid[i][j] == 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+}
