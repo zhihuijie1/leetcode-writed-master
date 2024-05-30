f4c8f737eca92fff6c5edd55ba06b49e0618bb95
yibai <404223879@qq.com>
2024-05-31 02:26:43 +0800

    N11

---
+++ top100/N11.java
@@ -0,0 +1,23 @@
+package algorithmbasic.leetcode.top100;
+
+public class N11 {
+    public int maxArea(int[] height) {
+        //双指针法从两边向内收缩
+        int N = height.length;
+        int left = 0;
+        int right = N - 1;
+        int maxt = 0;
+
+        while(left < right) {
+            int s = Math.min(height[left],height[right]) * (right - left);
+            maxt = Math.max(maxt, s);
+            //短边移动 因为尽可能的大
+            if(height[left] > height[right]) {
+                right--;
+            }else {
+                left++;
+            }
+        }
+        return maxt;
+    }
+}
